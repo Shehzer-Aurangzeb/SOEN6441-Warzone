e1c85e1b942a2b5a2a616e66706d6b429b009f3b
shehzar-abbasi <shehzar@dechains.com>
2024-02-16 18:47:29 -0500

    map view test added

---
+++ src/test/java/views/MapView/MapViewTest.java
@@ -0,0 +1,23 @@
+package views.MapView;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MapViewTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void displayMapInformation() {
+
+    }
+}
