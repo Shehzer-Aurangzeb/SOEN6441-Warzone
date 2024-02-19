e2fec18af5a96f1648585bb86f4ffb47735de837
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2024-02-18 19:15:18 -0500

    Updated MapValidator logics and MapValidatorTest file

--- src/test/java/models/Map/MapValidatorTest.java
+++ src/test/java/models/Map/MapValidatorTest.java
@@ -1,7 +1,5 @@
 package models.Map;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,34 +7,31 @@ import models.Continent.Continent;
 import models.Country.Country;
 
 public class MapValidatorTest {
-    @BeforeEach
-    void setUp() {
-    }
 
 
     @Test
-    public void testValidMap() {
+    void ValidMap() {
         // Create a valid map with connected graph, connected continents, and unique country names
         Map map = createValidMap();
         assertTrue(MapValidator.validateMap(map));
     }
 
     @Test
-    public void testInvalidMapDisconnectedGraph() {
+    void InvalidMapDisconnectedGraph() {
         // Create an invalid map with a disconnected graph
         Map map = createInvalidMapDisconnectedGraph();
         assertFalse(MapValidator.validateMap(map));
     }
 
     @Test
-    public void testInvalidMapDisconnectedContinent() {
+    void InvalidMapDisconnectedSubgraphs() {
         // Create an invalid map with a disconnected continent
-        Map map = createInvalidMapDisconnectedContinent();
+        Map map = createInvalidMapDisconnectedSubgraphs();
         assertFalse(MapValidator.validateMap(map));
     }
 
     @Test
-    public void testInvalidMapDuplicateCountryName() {
+    void testInvalidMapDuplicateCountryName() {
         // Create an invalid map with duplicate country names
         Map map = createInvalidMapDuplicateCountryName();
         assertFalse(MapValidator.validateMap(map));
@@ -45,13 +40,11 @@ public class MapValidatorTest {
     private Map createValidMap() {
         Map map = new Map();
 
-        // Create continents
         Continent continent1 = new Continent("Continent1", 3);
         Continent continent2 = new Continent("Continent2", 2);
         map.addContinent(continent1);
         map.addContinent(continent2);
 
-        // Create countries
         Country country1 = new Country(1, "Country1", 1);
         Country country2 = new Country(2, "Country2", 1);
         Country country3 = new Country(3, "Country3", 2);
@@ -65,7 +58,6 @@ public class MapValidatorTest {
         country3.addNeighbor(country4);
         country4.addNeighbor(country3);
 
-        // Add countries to the map
         map.addCountry(country1);
         map.addCountry(country2);
         map.addCountry(country3);
@@ -77,54 +69,70 @@ public class MapValidatorTest {
     private Map createInvalidMapDisconnectedGraph() {
         Map map = new Map();
 
-        // Create a continent
-        Continent continent = new Continent("Continent1", 3);
-        map.addContinent(continent);
+        Continent continent1 = new Continent("Continent1", 3);
+        Continent continent2 = new Continent("Continent2", 2);
+        Continent continent3 = new Continent("Continent3", 5);
+        map.addContinent(continent1);
+        map.addContinent(continent2);
+        map.addContinent(continent3);
 
-        // Create countries
         Country country1 = new Country(1, "Country1", 1);
         Country country2 = new Country(2, "Country2", 1);
-        Country country3 = new Country(3, "Country3", 1);
+        Country country3 = new Country(3, "Country3", 2);
+        Country country4 = new Country(4, "Country4", 2);
+        Country country5 = new Country(5, "Country5", 2);
+        Country country6 = new Country(6, "Country6", 3);
 
-        // Connect country1 and country2, but country3 is not connected
+        // Continent1 and Continent2 are not connected
         country1.addNeighbor(country2);
         country2.addNeighbor(country1);
 
-        // Add countries to the map
+        country3.addNeighbor(country4);
+        country4.addNeighbor(country3);
+        country4.addNeighbor(country5);
+        country5.addNeighbor(country4);
+
+        country6.addNeighbor(country1);
+        country1.addNeighbor(country6);
+
+
         map.addCountry(country1);
         map.addCountry(country2);
         map.addCountry(country3);
+        map.addCountry(country4);
+        map.addCountry(country5);
+        map.addCountry(country6);
 
         return map;
     }
 
-    private Map createInvalidMapDisconnectedContinent() {
+    private Map createInvalidMapDisconnectedSubgraphs() {
         Map map = new Map();
 
-        // Create continents
         Continent continent1 = new Continent("Continent1", 3);
         Continent continent2 = new Continent("Continent2", 2);
         map.addContinent(continent1);
         map.addContinent(continent2);
 
-        // Create countries
         Country country1 = new Country(1, "Country1", 1);
         Country country2 = new Country(2, "Country2", 1);
         Country country3 = new Country(3, "Country3", 2);
         Country country4 = new Country(4, "Country4", 2);
+        Country country5 = new Country(5, "Country5", 2);
 
-        // Connect country1 and country2 in Continent1
+        // Connect country4 and country5 are not connected
         country1.addNeighbor(country2);
         country2.addNeighbor(country1);
         country2.addNeighbor(country3);
         country3.addNeighbor(country2);
+        country3.addNeighbor(country4);
+        country4.addNeighbor(country3);
 
-
-        // Add countries to the map
         map.addCountry(country1);
         map.addCountry(country2);
         map.addCountry(country3);
         map.addCountry(country4);
+        map.addCountry(country5);
 
         return map;
     }
@@ -132,11 +140,9 @@ public class MapValidatorTest {
     private Map createInvalidMapDuplicateCountryName() {
         Map map = new Map();
 
-        // Create continents
         Continent continent1 = new Continent("Continent1", 3);
         map.addContinent(continent1);
 
-        // Create countries
         Country country1 = new Country(1, "Country1", 1);
         Country country2 = new Country(2, "Country2", 1);
         Country duplicateCountry = new Country(3, "Country1", 1); // Duplicate name
@@ -147,7 +153,6 @@ public class MapValidatorTest {
         country2.addNeighbor(duplicateCountry);
         duplicateCountry.addNeighbor(country2);
 
-        // Add countries to the map
         map.addCountry(country1);
         map.addCountry(country2);
         map.addCountry(duplicateCountry);
